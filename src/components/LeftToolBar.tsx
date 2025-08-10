b24c80c2871d6d64f63295f5ff99f1c018cf5c5f
MAC <mac@192.168.2.202>
2025-08-10 14:38:39 +0700

    [feat] real crop image

--- src/components/LeftToolBar.tsx
+++ src/components/LeftToolBar.tsx
@@ -1,46 +1,85 @@
-// import { useImageEditor } from '../../hooks/useImageEditor';
-// import { useImageEditorStore } from '../../store/imageEditor.store';
-
 import { Button, Tooltip } from "antd";
 import { useImageEditor } from "../hooks/useImageEditor";
 import { useImageEditorStore } from "../store/imageEditorStore";
-import { RotateLeftOutlined, RotateRightOutlined, ZoomInOutlined, ZoomOutOutlined } from "@ant-design/icons";
+import {
+  RotateLeftOutlined,
+  RotateRightOutlined,
+  ZoomInOutlined,
+  ZoomOutOutlined,
+} from "@ant-design/icons";
 
 const LeftToolBar = () => {
-    const { canvasRef } = useImageEditorStore();
-    const { handleZoomIn, handleZoomOut, handleRotateLeft, handleRotateRight } = useImageEditor();
+  const { canvasRef, setCropRec, canvasOfCropRecRef } = useImageEditorStore();
+  const {
+    handleZoomIn,
+    handleZoomOut,
+    handleRotateLeft,
+    handleRotateRight,
+    startCropMode,
+    downloadCroppedImage,
+    applyRealCropToActiveImage,
+    stopCropMode,
+  } = useImageEditor();
 
-    if (!canvasRef) return null;
+  if (!canvasRef) return null;
 
-    return (
-        <div className="flex flex-wrap bg-ColorToken-secondaryToken-300">
-            <Tooltip title="Zoom In">
-                <Button
-                    icon={<ZoomInOutlined />}
-                    onClick={() => handleZoomIn(canvasRef)}
-                ></Button>
-            </Tooltip>
-            <Tooltip title="Zoom Out">
-                <Button
-                    icon={<ZoomOutOutlined />}
-                    onClick={() => handleZoomOut(canvasRef)}
-                ></Button>
-            </Tooltip>
-            <Tooltip title="Rotate Left">
-                <Button
-                    icon={<RotateLeftOutlined />}
-                    onClick={() => handleRotateLeft(canvasRef)}
-                ></Button>
-            </Tooltip>
-            <Tooltip title="Rotate right">
-                <Button
-                    icon={<RotateRightOutlined />}
-                    onClick={() => handleRotateRight(canvasRef)}
-                ></Button>
-            </Tooltip>
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap bg-ColorToken-secondaryToken-300">
+      <Tooltip title="Zoom In">
+        <Button
+          icon={<ZoomInOutlined />}
+          onClick={() => handleZoomIn(canvasRef)}
+        ></Button>
+      </Tooltip>
+      <Tooltip title="Zoom Out">
+        <Button
+          icon={<ZoomOutOutlined />}
+          onClick={() => handleZoomOut(canvasRef)}
+        ></Button>
+      </Tooltip>
+      <Tooltip title="Rotate Left">
+        <Button
+          icon={<RotateLeftOutlined />}
+          onClick={() => handleRotateLeft(canvasRef)}
+        ></Button>
+      </Tooltip>
+      <Tooltip title="Rotate right">
+        <Button
+          icon={<RotateRightOutlined />}
+          onClick={() => handleRotateRight(canvasRef)}
+        ></Button>
+      </Tooltip>
+      <div>
+        <Button onClick={() => startCropMode(canvasRef, setCropRec)}>
+          Start Crop Rect
+        </Button>
+
+        <Button
+          onClick={() =>
+            stopCropMode(canvasRef, canvasOfCropRecRef, setCropRec)
+          }
+        >
+          Stop Crop Rect
+        </Button>
+
+        <Button
+          onClick={() =>
+            applyRealCropToActiveImage(
+              canvasRef,
+              canvasOfCropRecRef,
+              setCropRec
+            )
+          }
+        >
+          Apply Real Crop (clip)
+        </Button>
+
+        <Button onClick={() => downloadCroppedImage(canvasRef)}>
+          Download
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LeftToolBar;
- 
